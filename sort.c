bd77702eddc732469118e0ab75ff95d761115fab
yyc8891 <yyc8891@qq.com>
2020-02-21 19:46:50 +0800

    add insert sort

--- sort.c
+++ sort.c
@@ -13,7 +13,49 @@ void PrintArry(int A[],int num)
 	
 }
 
-int pop_sort(int A[],int num)
+void insert_sort(int A[],int num)
+{
+    int i = 0;
+    int j = 0;
+    int k =0;
+    int tmp = 0;
+    int min = 0;
+    
+    for(i=1;i<num;i++)
+    {
+        j=i-1;
+        if(A[i]<=A[j])
+        {
+            while(j>=0)
+            {
+                if(A[j]<A[i])
+                {
+                    break;
+                }
+                j--;
+            }
+            
+            j++;
+                
+            printf("\n get i:%d j:%d\n",i,j);
+            k=i;
+            min = A[i];
+            while(k>j)
+            {
+                tmp=A[k];
+                A[k]=A[k-1];
+                k--;
+            }
+            A[j]=min;
+        }
+        
+        PrintArry(A,10);
+            
+    }
+    
+}
+
+void pop_sort(int A[],int num)
 {
 	int i = 0;
 	int j = 0;
@@ -41,7 +83,8 @@ int pop_sort(int A[],int num)
 	}	
 	
 }
-int select_sort(int A[],int num)
+
+void select_sort(int A[],int num)
 {
 	int tmp = 0;
 	
@@ -124,9 +167,10 @@ int fast_sort(int A[],int top,int end)
 		fast_sort(A,top,i);
 		fast_sort(A,i+1,end);
 	}
+    return 0;
 }
 
-main(void)
+int main(void)
 {
 	int Arry[10]={34,19,234,67,1,111,5,734,50,92};	
 	int ret = 0;
@@ -138,9 +182,12 @@ main(void)
 	//printf("\n test for select sort \n");
 	//select_sort(Arry,10);
 	
-	printf("\n test for pop sort \n");
-	pop_sort(Arry,10);
+	//printf("\n test for pop sort \n");
+	//pop_sort(Arry,10);
 	
+    printf("\n test for insert sort \n");
+    insert_sort(Arry,10);
+    
 	//printf("\n ret : %d \n",ret);
 	PrintArry(Arry,10);
 }
